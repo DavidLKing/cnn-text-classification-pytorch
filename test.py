96ac80663a28cdde585ee079877bc78aea70e004
David L. King <king.david.lee@gmail.com>
2018-02-21 20:41:40 -0500

    cleaning up

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import sys
 import subprocess
+import pdb
 
 for line in open(sys.argv[1]):
     line = line.strip()
-    cmd = ['./main.py', '-predict="' + line + '"']
+    # cmd = ['./main.py', '-predict="' + line + '"']
+    cmd = ['./main.py', str('-snapshot="snapshot/2018-02-21_16-48-56/best_steps_1500.pt"'),
+           '-kernel-sizes', '1,2,3,4,5', '-predict="' + line + '"']
+    pdb.set_trace()
     # cmd += line
     # cmd += '"'
     subprocess.run(cmd)
